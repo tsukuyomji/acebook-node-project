3e4877c4d003c93516f2814c1ba4e6aaaeb1f947
Anton Edeh <87416347+tsukuyomji@users.noreply.github.com>
2026-08-11 10:35:53 +0100

    Initial commit

---
+++ models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  message: String,
+  likes: { type: Number, default: 0 }
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
